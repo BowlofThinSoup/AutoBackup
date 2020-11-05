d5431ee6a0c636ad70626cee5d2a99230a178d26
QingTang <2991855418@qq.com>
2020-11-05 16:05:27 +0800

    use APScheduler for time saving

---
+++ Config.py
@@ -0,0 +1,4 @@
+src_dir = 'E:/test'
+dst_dir = "E:/backup"
+md5file = 'E:/backup/md5.data'
+log_file = 'E:/backup/log.txt'
--- Logger.py
+++ Logger.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 import logging
@@ -14,7 +15,20 @@ if __name__ == "__main__":
     src_dir = 'E:/test'
     dst_dir = "E:/backup"
     md5file = 'E:/backup/md5.data'
-    
+    log_file = 'E:/backup/log.txt'
+    # src_dir = sys.argv[1]if len(sys.argv) > 1 else '.'
+    # dst_dir = sys.argv[2]if len(sys.argv) > 2 else './backup'
+    # md5file = sys.argv[1]+'/md5.data'if len(sys.argv) > 1 else './md5.data'
+    # log_file = sys.argv[1]+'/log.txt'if len(sys.argv) > 1 else './log.txt'
+
+    logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    filename=log_file)
+    b.full_backup(src_dir,dst_dir,md5file)
+    print("ok")
+
+
     observer = Observer()
     event_handler = backup()
     observer.schedule(event_handler,src_dir,True)
